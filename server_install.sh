57eedda5907c8c02847627443a88c7828f83875b
Mohamed M. Hagag <mohamedhagag@users.noreply.github.com>
2024-12-17 01:17:07 +0300

    fix odoo conf addons path

--- server_install.sh
+++ server_install.sh
@@ -377,7 +377,7 @@ cd $ODIR && source $BWS/bin/activate && ./odoo/odoo-bin --without-demo=all -c $O
 echo "Creating odoo config file ..."
 cat <<EOF >$ODIR/Odoo.conf
 [options]
-addons_path = ~/odoo/odoo/addons,~/odoo/addons,~/my_adds/${AUSR},~/my_adds/community,~/my_adds/enterprise
+addons_path = ${ODIR}/odoo/odoo/addons,${ODIR}/odoo/addons,${ODIR}/my_adds/${AUSR},${ODIR}/my_adds/community,${ODIR}/my_adds/enterprise
 admin_passwd = 123@admin
 xmlrpc_port = ${PORT1}
 longpolling_port = ${PORT2}
@@ -388,6 +388,7 @@ workers = 4
 EOF
 
 mkdir -p $ODIR/my_adds/{enterprise,community}
+mkdir -p $ODIR/my_adds/${AUSR}
 
 echo asn1crypto >> $RQF
 echo phonenumbers >> $RQF
